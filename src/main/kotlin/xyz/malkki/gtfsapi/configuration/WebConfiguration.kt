dfb522a2eaf419eac17948d7d83d87e26f7abd5b
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-08-20 23:21:52 +0300

    Add common Spring configurations

---
+++ src/main/kotlin/xyz/malkki/gtfsapi/configuration/WebConfiguration.kt
@@ -0,0 +1,14 @@
+package xyz.malkki.gtfsapi.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
