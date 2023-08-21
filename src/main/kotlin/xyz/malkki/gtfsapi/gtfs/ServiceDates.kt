22095efc5dbe0e80e8550c7eb6e7a07407ea7225
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-08-21 21:42:52 +0300

    Create helper class for getting dates when the service operatres

---
+++ src/main/kotlin/xyz/malkki/gtfsapi/gtfs/ServiceDates.kt
@@ -0,0 +1,42 @@
+package xyz.malkki.gtfsapi.gtfs
+
+import com.github.benmanes.caffeine.cache.Caffeine
+import xyz.malkki.gtfs.model.Calendar
+import xyz.malkki.gtfs.model.CalendarDate
+import xyz.malkki.gtfsapi.common.DateSet
+import java.time.LocalDate
+import java.util.*
+
+class ServiceDates(calendars: Collection<Calendar>, calendarDates: Collection<CalendarDate>) {
+    private val calendarsByServiceId = calendars.groupBy { it.serviceId }
+    private val calendarDatesByServiceId = calendarDates.groupBy { it.serviceId }
+
+    private val dateSetCache = Caffeine.newBuilder()
+        .maximumWeight(15 * 1024 * 1024 * 8) //15MB
+        .weigher { _: String, set: DateSet ->
+            set.bitSize
+        }
+        .build<String, DateSet> { serviceId ->
+            DateSet(getDatesForService(serviceId))
+        }
+
+    private fun getDatesForService(serviceId: String): Collection<LocalDate> {
+        val dates = (calendarsByServiceId[serviceId] ?: emptyList())
+            .flatMap { it.toList() }
+            .toList()
+
+        val calendarDates = calendarDatesByServiceId[serviceId] ?: emptyList()
+
+        val addedDates = calendarDates
+            .filter { it.exceptionType == CalendarDate.EXCEPTION_TYPE_ADDED }
+            .map { it.date }
+        val removedDates = calendarDates
+            .filter { it.exceptionType == CalendarDate.EXCEPTION_TYPE_REMOVED }
+            .map { it.date }
+            .toSet()
+
+        return (dates + addedDates).filter { it !in removedDates }
+    }
+
+    fun getDatesForServiceId(serviceId: String): NavigableSet<LocalDate> = dateSetCache[serviceId]
+}
