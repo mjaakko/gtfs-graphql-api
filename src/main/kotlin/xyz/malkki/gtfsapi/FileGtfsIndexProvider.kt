24fcb0eb2597b1e530bc198ae28ee98644acf910
Jaakko Malkki <jaakko.malkki234@gmail.com>
2023-10-18 22:55:22 +0300

    Create interface for GtfsIndexProvider

---
+++ src/main/kotlin/xyz/malkki/gtfsapi/FileGtfsIndexProvider.kt
@@ -0,0 +1,49 @@
+package xyz.malkki.gtfsapi
+
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Component
+import xyz.malkki.gtfs.serialization.parser.ZipGtfsFeedParser
+import xyz.malkki.gtfsapi.extensions.debounce
+import xyz.malkki.gtfsapi.gtfs.GtfsIndex
+import xyz.malkki.gtfsapi.utils.watchFile
+import java.nio.file.Paths
+import java.nio.file.StandardWatchEventKinds
+import java.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
+
+
+@Component
+@ConditionalOnProperty(name = ["gtfs.feed.path"])
+class FileGtfsIndexProvider(@Value("\${gtfs.feed.path}") gtfsPathString: String) : GtfsIndexProvider {
+    private val gtfsPath = Paths.get(gtfsPathString)
+
+    override val gtfsIndexFlux = gtfsPath.watchFile(StandardWatchEventKinds.ENTRY_MODIFY)
+        .debounce(500.milliseconds)
+        .map {  }
+        .startWith(Unit)
+        .map {
+            val gtfsParser = ZipGtfsFeedParser(gtfsPath)
+
+            gtfsParser.use {
+                GtfsIndex(
+                    it.parseAgencies().toList(),
+                    it.parseRoutes().toList(),
+                    it.parseTrips().toList(),
+                    it.parseStops().toList(),
+                    it.parseStopTimes().toList(),
+                    it.parseCalendars().toList(),
+                    it.parseCalendarDates().toList(),
+                    it.parseShapes().toList()
+                )
+            }
+        }
+        .replay(1)
+        .refCount()
+
+    @PostConstruct
+    fun waitForIndex() {
+        gtfsIndexFlux.blockFirst(Duration.ofMinutes(5))
+    }
+}
